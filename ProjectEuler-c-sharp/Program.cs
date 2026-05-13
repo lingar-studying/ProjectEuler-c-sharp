8d240316baf751468f509e0d67a136f3743b3430
Your Name <you@example.com>
2026-05-13 17:23:23 +0300

    Add project files.

---
+++ ProjectEuler-c-sharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProjectEuler_c_sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
